546b572935675624535e388b2d096815090092c7
AAArisle <2416948889@qq.com>
2024-04-19 15:37:47 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// app.js
+App({
+  onLaunch() {
+    // 这缓存清不掉啊
+    wx.clearStorageSync()
+    wx.removeStorage()
+
+    // 登录
+    // this.login()
+
+    // 展示本地存储能力
+    // const logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+  },
+  // globalData: {
+  //   userInfo: null
+  // }
+
+  // 返回布尔值，检查token是否过期
+  isTokenAvailable() {
+    const now = Date.parse(new Date());
+    //同步获取token有效时间
+    const accessTime = wx.getStorageSync('access_time')
+    if ((accessTime !== '') && (now - accessTime < 5 * 60 * 1000)) {
+      return true
+    }
+    return false
+  },
+
+  getToken(callback) {
+    console.log('app：从django获得token中')
+    wx.login({
+      success(res) {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            // 这里是django的本地ip地址
+            // 如果部署到线上，需要改为接口的实际网址
+            url: 'http://127.0.0.1:8000/api/weixin/login/',
+            method: 'POST',
+            data: {
+              code: res.code
+            },
+            success: res => {
+              // 在小程序调试器中查看是否收到token
+              console.log(res)
+              const access = res.data.access
+              // 将token保存到缓存
+              wx.setStorage({ key: "access", data: access, 
+                success: ()=> {
+                  console.log('app：token存储成功');
+                  // 调用callback
+                  // 以便在登录完成后执行后续动作
+                  callback()
+                } 
+              })
+              // 保存token的获取时间
+              wx.setStorage({
+                key: "access_time",
+                data: Date.parse(new Date())
+              })
+            }
+          })
+        }
+        else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  },
+
+  // 登录函数
+  login(callback = (() => {})) {
+    if (!this.isTokenAvailable()) {
+      console.log('app：从django获得token')
+      // 获取token并传入callback
+      this.getToken(callback)
+    } else {
+      console.log('app：从本地缓存获得token')
+      // 如果缓存中token未过期则直接执行callback
+      callback()
+    }
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,107 @@
+// index.js
+const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const app = getApp()
+
+Page({
+  onReady() {
+    app.login(() => {
+      // 用token获取用户数据
+      console.log('login：从django获得data中')
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          console.log('login：token获得成功');
+          console.log(access);
+          wx.request({
+            url: 'http://127.0.0.1:8000/api/weixin/data/',
+            header: {
+              // 注意字符串 'Bearer ' 尾部有个空格！
+              'Authorization': 'Bearer ' + access
+            },
+            success: res => {
+              // 在小程序调试器中查看返回值是否正确
+              console.log(res)
+              this.setData({
+                "userInfo.avatarUrl": res.data.avatar_url,
+                "userInfo.nickName": res.data.nickname,
+                hasUserInfo: res.data.nickname && res.data.avatar_url && res.data.avatar_url !== defaultAvatarUrl,
+              })
+            }
+          })
+        }
+      });
+    })
+  },
+
+  data: {
+    userInfo: {
+      avatarUrl: defaultAvatarUrl,
+      nickName: '',
+    },
+    hasUserInfo: false,
+    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
+    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+  },
+
+  // 将清单数据提交到django
+  uploadData() {
+    if (this.data.hasUserInfo){
+      app.login(() => {
+        wx.getStorage({
+          key: 'access',
+          success: (result) => {
+            const access = result.data;
+            wx.request({
+              url: 'http://127.0.0.1:8000/api/weixin/data/',
+              method: 'POST',
+              header: {
+                'Authorization': 'Bearer ' + access
+              },
+              data: {
+                userInfo: this.data.userInfo
+              }
+            })
+          }
+        })
+      })
+    }
+  },
+
+  bindViewTap() {
+    wx.switchTab({
+      url: '../index/index'
+    })
+  },
+  onChooseAvatar(e) {
+    const { avatarUrl } = e.detail
+    const { nickName } = this.data.userInfo
+    this.setData({
+      "userInfo.avatarUrl": avatarUrl,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+    this.uploadData()
+  },
+  onInputChange(e) {
+    const nickName = e.detail.value
+    const { avatarUrl } = this.data.userInfo
+    this.setData({
+      "userInfo.nickName": nickName,
+      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+    })
+    this.uploadData()
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+})
