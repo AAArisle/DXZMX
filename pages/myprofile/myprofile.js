a3a32eec90f66da6b496e88d57d0aff3920be89d
AAArisle <2416948889@qq.com>
2024-05-14 23:39:43 +0800

    分享经验的前端数据接口，个人资料详情页增加保存按钮

--- pages/myprofile/myprofile.js
+++ pages/myprofile/myprofile.js
@@ -1,35 +1,34 @@
 // pages/myprofile/myprofile.js
-
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const app = getApp()
 
 Page({
   onReady() {
-    wx.getStorage({
-      key: 'access',
-      success: (result) => {
-        const access = result.data;
-        wx.request({
-          url: 'http://127.0.0.1:8000/api/weixin/data/',
-          header: {
-            // 注意字符串 'Bearer ' 尾部有个空格！
-            'Authorization': 'Bearer ' + access
-          },
-          success: res => {
-            // 在小程序调试器中查看返回值是否正确
-            this.setData({
-              "userInfo.avatarUrl": res.data.avatar_url,
-              "userInfo.nickName": res.data.nickname,
-            })
-          }
-        })
-      }
-    });
+    app.login(() => {
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          wx.request({
+            url: 'http://127.0.0.1:8000/api/weixin/data/',
+            header: {
+              'Authorization': 'Bearer ' + access
+            },
+            success: res => {
+              this.setData({
+                "userInfo.avatarUrl": res.data.avatar_url,
+                "userInfo.nickName": res.data.nickname,
+              })
+            }
+          })
+        }
+      });
+    })
   },
   onChooseAvatar(e) {
     this.setData({
       "userInfo.avatarUrl": e.detail,
     })
-    this.uploadData()
   },
   onNickNameInput(e) {
     const nickName = e.detail.value
@@ -37,25 +36,26 @@ Page({
       this.setData({
         "userInfo.nickName": nickName,
       })
-      this.uploadData()
     }
   },
   uploadData() {
-    wx.getStorage({
-      key: 'access',
-      success: (result) => {
-        const access = result.data;
-        wx.request({
-          url: 'http://127.0.0.1:8000/api/weixin/data/',
-          method: 'POST',
-          header: {
-            'Authorization': 'Bearer ' + access
-          },
-          data: {
-            userInfo: this.data.userInfo
-          }
-        })
-      }
+    app.login(() => {
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          wx.request({
+            url: 'http://127.0.0.1:8000/api/weixin/data/',
+            method: 'POST',
+            header: {
+              'Authorization': 'Bearer ' + access
+            },
+            data: {
+              userInfo: this.data.userInfo
+            }
+          })
+        }
+      })
     })
   },
   onClickUserMessageEdit(){
@@ -71,53 +71,4 @@ Page({
       nickName: '',
     },
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
