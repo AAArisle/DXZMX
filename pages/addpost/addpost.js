f94aa1e03b4436a63ea4d83a592f3c0cd960b210
AAArisle <2416948889@qq.com>
2024-05-21 23:27:48 +0800

    修改上传图片的占位图片，增加分享经验时图片的上传功能，分享成功后会重置整个表单

--- pages/addpost/addpost.js
+++ pages/addpost/addpost.js
@@ -4,7 +4,11 @@ const app = getApp()
 Page({
   // 页面的初始数据
   data: {
-    mypath: '../../images/upload.png',
+    article: {
+      title:'',
+      body:'',
+      mypath: '../../images/upload.png',
+    },
     school:["清华大学","北京大学"],
     schooldata:'请选择',
     zhuanye:["数学系","计算机系"],
@@ -29,20 +33,20 @@ Page({
     })
   },
   upload:function(){
-    var that = this
     wx.chooseMedia({
       count:1,
       mediaType:["image"],
       sourceType:['album', 'camera'],
       success:res=>{
-        const path = res.tempFiles.tempFilePath
+        const path = res.tempFiles[0].tempFilePath;
+        console.log(path);
         if(path != ''){
           wx.showToast({
-            title: '上传图片成功',
-            icon:'none'
+            title: '添加图片成功',
+            icon:'success'
           })
-          that.setData({
-            mypath : path
+          this.setData({
+            "article.mypath" : path
           })
         }
       }
@@ -82,33 +86,52 @@ Page({
       console.log("正在上传...");
       wx.showToast({
         title: '分享经验中',
-        duration: 2000,
         icon: 'loading',
-        mask: true
       })
       app.login(() => {
         wx.getStorage({
           key: 'access',
           success: (result) => {
             const access = result.data;
-            wx.request({
-              url: 'http://127.0.0.1:8000/api/article/article-create/',
-              method: 'POST',
-              header: {
-                'Authorization': 'Bearer ' + access,
-                'content-type': 'application/x-www-form-urlencoded',
-              },
-              data: formData, //表单数据
-              success: () => {
-                wx.showToast({
-                  title: '分享经验成功！', //提示的内容
-                  duration: 2000, //持续的时间
-                  icon: 'success', //图标有success、error、loading、none四种
-                  mask: true //显示透明蒙层 防止触摸穿透
-                });
-                this.formReset();
-              }
-            })
+            if (this.data.article.mypath == '../../images/upload.png') {
+              // 如果没有图片
+              wx.request({
+                url: 'http://127.0.0.1:8000/api/article/article-create/',
+                method: 'POST',
+                header: {
+                  'Authorization': 'Bearer ' + access,
+                  'content-type': 'application/x-www-form-urlencoded',
+                },
+                data: formData, //表单数据
+                success: () => {
+                  wx.showToast({
+                    title: '分享经验成功！',
+                    icon: 'success',
+                  });
+                  this.formReset();
+                }
+              })
+            }
+            else {
+              // 有图片
+              wx.uploadFile({
+                filePath: this.data.article.mypath,
+                name: 'image',
+                url: 'http://127.0.0.1:8000/api/article/article-create/',
+                formData: formData,
+                header: {
+                  'Authorization': 'Bearer ' + access,
+                  'content-type': 'application/x-www-form-urlencoded',
+                },
+                success: () => {
+                  wx.showToast({
+                    title: '分享经验成功！',
+                    icon: 'success',
+                  });
+                  this.formReset();
+                }
+              })
+            }
           }
         })
       })
@@ -124,7 +147,13 @@ Page({
   },
 
   // 重置表单
-  formReset(e) {
-    console.log('form发生了reset事件，携带数据为：', e.detail.value)
+  formReset() {
+    console.log('form发生了reset事件')
+    this.setData({
+      "article.title": '',
+      "article.body": '',
+      "article.mypath": '../../images/upload.png',
+      
+    })
   }
 })
