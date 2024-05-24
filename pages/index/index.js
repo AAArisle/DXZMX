d2b7404a032d507d988ee56c36b0ab75852b3d58
AAArisle <2416948889@qq.com>
2024-05-25 01:05:31 +0800

    添加首页的经验列表显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,37 @@
 // pages/top.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+const app = getApp()
 
 Page({
+  onReady() {
+    app.login(() => {
+      // 用token获取用户数据
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          console.log('index：token获得成功');
+          wx.request({
+            url: 'http://127.0.0.1:8000/api/article/article-list/',
+            method: 'GET',
+            header: {
+              // 注意字符串 'Bearer ' 尾部有个空格！
+              'Authorization': 'Bearer ' + access
+            },
+            success: res => {
+              // 在小程序调试器中查看返回值是否正确
+              let articles = JSON.parse(res.data.articles)
+              console.log(articles)
+              this.setData({
+                article: articles
+              })
+            }
+          })
+        }
+      });
+    })
+  },
+
+  
   tapToSearch: function() {
     wx.navigateTo({
       url: '../search/search',
@@ -17,67 +47,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    rowColsAvater: [{ size: '96rpx', type: 'circle' }],
-    rowColsImage: [{ size: '96rpx', type: 'rect' }],
-    rowColsContent: [{ width: '50%' }, { width: '100%' }],
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-    },
+    article:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    app.login(() => {
+      // 用token获取用户数据
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          console.log('index：token获得成功');
+          wx.request({
+            url: 'http://127.0.0.1:8000/api/article/article-list/',
+            method: 'GET',
+            header: {
+              // 注意字符串 'Bearer ' 尾部有个空格！
+              'Authorization': 'Bearer ' + access
+            },
+            success: res => {
+              // 在小程序调试器中查看返回值是否正确
+              let articles = JSON.parse(res.data.articles)
+              console.log(articles)
+              this.setData({
+                article: articles
+              })
+            }
+          })
+        }
+      });
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
