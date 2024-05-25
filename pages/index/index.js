4df1223cb84a2776c6d8b313fa1a159a1745d79b
AAArisle <2416948889@qq.com>
2024-05-25 17:14:48 +0800

    取消添加经验的正文字数限制，修改添加经验界面。首页添加浏览量、评论数（还没做）、点赞数的显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,37 +1,7 @@
 // pages/top.js
 const app = getApp()
 
-Page({
-  onReady() {
-    app.login(() => {
-      // 用token获取用户数据
-      wx.getStorage({
-        key: 'access',
-        success: (result) => {
-          const access = result.data;
-          console.log('index：token获得成功');
-          wx.request({
-            url: 'http://127.0.0.1:8000/api/article/article-list/',
-            method: 'GET',
-            header: {
-              // 注意字符串 'Bearer ' 尾部有个空格！
-              'Authorization': 'Bearer ' + access
-            },
-            success: res => {
-              // 在小程序调试器中查看返回值是否正确
-              let articles = JSON.parse(res.data.articles)
-              console.log(articles)
-              this.setData({
-                article: articles
-              })
-            }
-          })
-        }
-      });
-    })
-  },
-
-  
+Page({  
   tapToSearch: function() {
     wx.navigateTo({
       url: '../search/search',
@@ -49,10 +19,11 @@ Page({
   data: {
     article:[]
   },
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
+
+    /**
+   * 生命周期函数--监听页面加载
    */
-  onPullDownRefresh() {
+  onLoad(options) {
     app.login(() => {
       // 用token获取用户数据
       wx.getStorage({
