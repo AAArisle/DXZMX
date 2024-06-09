a410fb5f8b29b430256433c12717a89139218fa5
AAArisle <2416948889@qq.com>
2024-06-09 20:29:52 +0800

    消息通知

--- pages/messages/messages.js
+++ pages/messages/messages.js
@@ -1,9 +1,11 @@
 // pages/messages/messages.js
+const app = getApp()
+
 Page({
-  tapToDetail()
-  {
+  tapToDetail(e) {
+    let id = e.currentTarget.dataset.id
     wx.navigateTo({
-      url: '../detailpost/detailpost',
+      url: '../detailpost/detailpost?id='+id,
     })
   },
   
@@ -45,55 +47,30 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    app.login(() => {
+      // 用token获取用户数据
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          console.log('index：token获得成功');
+          wx.request({
+            url: 'http://127.0.0.1:8000/notice/read/',
+            method: 'GET',
+            header: {
+              // 注意字符串 'Bearer ' 尾部有个空格！
+              'Authorization': 'Bearer ' + access
+            },
+            success: res => {
+              let notices = JSON.parse(res.data.notices)
+              console.log(notices)
+              this.setData({
+                notices: notices
+              })
+            }
+          })
+        }
+      });
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
--- pages/news/news.js
+++ pages/news/news.js
@@ -1,4 +1,6 @@
 // pages/news/news.js
+const app = getApp()
+
 Page({
   tapToMessages()
   {
@@ -6,10 +8,36 @@ Page({
       url: '../messages/messages',
     })
   },
+  tapToDetail(e) {
+    let id = e.currentTarget.dataset.id
+    let ntid = e.currentTarget.dataset.ntid
+    app.login(() => {
+      // 用token获取用户数据
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          console.log('index：token获得成功');
+          wx.request({
+            url: 'http://127.0.0.1:8000/notice/update/?notice_id='+ntid,
+            method: 'GET',
+            header: {
+              // 注意字符串 'Bearer ' 尾部有个空格！
+              'Authorization': 'Bearer ' + access
+            },
+          })
+        }
+      });
+    })
+    wx.navigateTo({
+      url: '../detailpost/detailpost?id='+id,
+    })
+  },
   /**
    * 页面的初始数据
    */
   data: {
+    notices:[],
     news: [{
       initiator_id: 1,
       name: "暨南大学",
@@ -25,62 +53,65 @@ Page({
       haveUnread: false,
       num: 1,
       date: "2024-5-20",
+    }],
+    initiator: {
+      name: "暨南大学",
+      icon: "../../images/大学/暨南大学.png",
+    },
+    messages: [{
+      id: 3,
+      date: "2024-5-20",
+      title: "新内容发布",
+      body: "点击前往详情界面"
+    },
+    {
+      id: 2,
+      date: "2024-5-20",
+      title: "新内容发布",
+      body: "点击前往详情界面"
+    },
+    {
+      id: 1,
+      date: "2024-5-20",
+      title: "新内容发布",
+      body: "点击前往详情界面"
+    },
+    {
+      id: 0,
+      date: "2024-5-20",
+      title: "新内容发布",
+      body: "点击前往详情界面"
     }]
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    app.login(() => {
+      // 用token获取用户数据
+      wx.getStorage({
+        key: 'access',
+        success: (result) => {
+          const access = result.data;
+          console.log('index：token获得成功');
+          wx.request({
+            url: 'http://127.0.0.1:8000/notice/list/',
+            method: 'GET',
+            header: {
+              // 注意字符串 'Bearer ' 尾部有个空格！
+              'Authorization': 'Bearer ' + access
+            },
+            success: res => {
+              let notices = JSON.parse(res.data.notices)
+              console.log(notices)
+              this.setData({
+                notices: notices
+              })
+            }
+          })
+        }
+      });
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
